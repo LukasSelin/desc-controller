d19b76a683108228af0d7a977461ddb827b0bfa0
Lukas Selin <lukas.selin@viskan.se>
2020-10-09 22:52:39 +0200

    asd

--- Desk_controller.py
+++ Desk_controller.py
@@ -12,43 +12,43 @@ currentHight = maxHight
 
 
 def initPorts():
-    GPIO.setup(upPort, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(downPart, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(controllPort, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(upPort, GPIO.IN)
+    GPIO.setup(downPart, GPIO.IN)
+ #   GPIO.setup(controllPort, GPIO.OUT, initial=GPIO.LOW)
 
 def calibrate():
     print("Started calibrating")
     GPIO.output(upPort, 1)
-    GPIO.output(controllPort, 1)
+    #GPIO.output(controllPort, 1)
 
     time.sleep(20)
 
     GPIO.output(upPort, 0)
-    GPIO.output(controllPort, 0)
+   # GPIO.output(controllPort, 0)
     print("Calibrate complete")
 
 
 def moveUp(offsetSeconds):
     print("Moving up for {} seconds".format(offsetSeconds))
     GPIO.output(upPort, 1)
-    GPIO.output(controllPort, 1)
+    #GPIO.output(controllPort, 1)
 
     time.sleep(offsetSeconds)
     
     GPIO.output(upPort, 0)
-    GPIO.output(controllPort, 0)
+    #GPIO.output(controllPort, 0)
     print("Stopped going up")
 
 def moveDown(offsetSeconds):
     print("Moving down for {} seconds".format(offsetSeconds))
     GPIO.output(downPart, 1)
-    GPIO.output(controllPort, 1)
+    #GPIO.output(controllPort, 1)
     offsetSeconds = offsetSeconds * -1
 
     time.sleep(offsetSeconds)
     
     GPIO.output(downPart, 0)
-    GPIO.output(controllPort, 0)
+    #GPIO.output(controllPort, 0)
     print("Stopped going down")
 
 def move(offsetHight):
